810ed9dd943d38ecf8936a11c5fd62fa0d183c9d
a7medking <a7medking@gmail.com>
2023-08-29 03:43:06 +0300

    ss

---
+++ lib/utils.dart
@@ -0,0 +1,11 @@
+
+
+
+
+
+const String merchantID = 'MIHOMHUCAQAwEzERMA8GA1UEAxMITWVuYSBTaW0wWTATBgcqhkjOPQIBBggqhkjOPQMBBwNCAASvt2KiDDtdaz2Oh7Aj754nD+S3hc3sU37Lqm6/5GMBb+NF4sSi8LmOu0hk3gxfawXoDLwJphRuu2CETtVsmTWQoAAwCgYIKoZIzj0EAwIDSQAwRgIhAMcYuiHf3bSPE6o+qK7RqGH8BefyfRsU+30mK9dc2PAiAiEAxpqULlv352d+mR/mq1YmPwWVcBfL3+aA2igpWipa+hI=';
+const String stripePublishableKey = 'pk_test_51N2IMOJd4F3GGcPqGWcRkUA2VzxijOOBgfuywYBKzuVp5swFtuWLZsLt15G3e8qPc7D5csMRDkuLQH6xLYRzpkm70043BuFJUG';
+const String stripeSecretKey = 'sk_test_51N2IMOJd4F3GGcPqP6EjvTuykTyoIgGycDeWxpyYle7ZLshbGPNAbinBxCvKfLW5uCJsIof1p8jgZMb8Lhm6FHf400CcRwY3RX';
+
+const String stripeBaseURL = 'https://api.stripe.com/';
+const String createPaymentIntentURL = 'v1/payment_intents';
